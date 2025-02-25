707c4b285dc6416a1532c025270305711528f578
Gian <gianmachellaf@gmail.com>
2025-02-25 12:11:22 -0600

    Separeta pojects

--- backend/models/index.js
+++
@@ -1,8 +0,0 @@
-const Client = require("./Client");
-const Shipment = require("./Shipment");
-
-// Definir las relaciones
-Client.hasMany(Shipment, { as: "shipments", foreignKey: "clientId" });
-Shipment.belongsTo(Client, { as: "client", foreignKey: "clientId" });
-
-module.exports = { Client, Shipment };
--- backend/routes/emailRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { sendEmail } = require("../controllers/emailController.js");
-
-const router = express.Router();
-
-router.post("/send-email", sendEmail);
-
-module.exports = router;
