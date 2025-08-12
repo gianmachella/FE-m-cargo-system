5d7c8082215309db743f1ea43b698bede2eb2787
Gian Machella <gianmachella@Gians-MBP.lan>
2025-08-12 14:06:56 -0500

    update styles for Login page

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,2 @@
-const API_BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? process.env.REACT_APP_API_URL
-    : process.env.REACT_APP_API_URL_PROD;
-
-console.log("API_BASE_URL:", API_BASE_URL);
+const API_BASE_URL = "/api";
 export default API_BASE_URL;
